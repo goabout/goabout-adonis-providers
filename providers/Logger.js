4b96ee83a571791c37a26975fa2f4b096043e7ce
Artem Golovin <agv.c1d@gmail.com>
2017-07-22 20:31:52 +0200

    Added logging to papertrail

--- providers/Logger.js
+++ providers/Logger.js
@@ -1,11 +1,29 @@
 const ServiceProvider = require('adonis-fold').ServiceProvider // eslint-disable-line
 const winston = require('winston')
 
-const initializeLogger = loggingLevel => {
+// Requiring `winston-papertrail` will expose `winston.transports.Papertrail`
+require('winston-papertrail').Papertrail //eslint-disable-line
+
+const initializeLogger = (loggingLevel, papertrailHost, papertrailPort, Raven) => {
+  const transports = [
+    new (winston.transports.Console)({ colorize: true })
+  ]
+
+  if (papertrailHost && papertrailPort) {
+    const winstonPapertrail = new winston.transports.Papertrail({
+      host: papertrailHost,
+      port: papertrailPort
+    })
+
+    winstonPapertrail.on('error', err => {
+      Raven.captureException(err)
+    })
+
+    transports.push(winstonPapertrail)
+  }
+
   const logger = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)({ colorize: true })
-    ],
+    transports,
     level: loggingLevel
   })
 
@@ -18,8 +36,11 @@ class LoggerProvider extends ServiceProvider {
   * register() {
     this.app.singleton('GoAbout/providers/Logger', () => {
       const Env = use('Env')
+      const Raven = use('Raven')
       const loggingLevel = Env.get('LOGGING', 'error')
-      return initializeLogger(loggingLevel)
+      const papertrailHost = Env.get('PAPERTRAIL_HOST', null)
+      const papertrailPort = Env.get('PAPERTRAIL_PORT', null)
+      return initializeLogger(loggingLevel, papertrailHost, papertrailPort, Raven)
     })
   }
 
