eaa39c1d0e1829fd9b9ef58f8ead9bdc25b70dc7
Artem Golovin <agv.c1d@gmail.com>
2017-08-07 17:39:23 +0200

    Added logInfo and logging about, sorry, logging

--- utils/Logger.js
+++ utils/Logger.js
@@ -6,12 +6,18 @@ const initializeLogger = (loggingLevel, papertrailHost, papertrailPort, papertra
     new (winston.transports.Console)({ colorize: true })
   ]
 
+  // Leave the 1st element for log leve;l
+  const logInfo = [null]
+
   if (papertrailHost && papertrailPort) {
     const winstonPapertrail = new Papertrail({
       host: papertrailHost,
       port: papertrailPort,
       hostname: papertrailHostname || undefined // Hostname is used to make sure that papertrail does not create a new logger each time new machine is up in the interface
     })
+
+
+    logInfo.push(`Logging to ${winstonPapertrail.host}:${winstonPapertrail.port} as ${winstonPapertrail.hostname}`)
 
     transports.push(winstonPapertrail)
   }
@@ -22,6 +28,11 @@ const initializeLogger = (loggingLevel, papertrailHost, papertrailPort, papertra
   })
 
   logger.cli()
+  logInfo[0] = `Log level is ${logger.level}`
+
+  logger.outputInfo = () => {
+    logInfo.forEach(info => logger.info(info))
+  }
 
   return logger
 }
