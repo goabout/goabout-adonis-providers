a56f5019c19a24ae9af359a65e6cc880e1f999f3
Artem Golovin <agv.c1d@gmail.com>
2017-11-01 17:33:36 +0100

    Properly add sessionIds to logs

--- utils/Logger.js
+++ utils/Logger.js
@@ -15,6 +15,20 @@ const initializeLogger = (loggingLevel, papertrailHost, papertrailPort, papertra
       port: papertrailPort,
       hostname: papertrailHostname || undefined, // Hostname is used to make sure that papertrail does not create a new logger each time new machine is up in the interface
       level: loggingLevel,
+      colorize: true,
+      logFormat(level, message) {
+        let formattedMessage = `${level}: ${message}`
+
+        if (CLS) {
+          const session = CLS.get('session')
+          const userUid = CLS.get('userUid')
+
+          const clsData = `${userUid ? userUid.slice(0, 10) : ''}${userUid && session ? '-' : ' '}${session || ''}`
+          if (clsData) formattedMessage = `\u001b[35m${clsData}\u001b[39m ${formattedMessage}`
+        }
+
+        return formattedMessage
+      }
     })
 
 
@@ -27,14 +41,6 @@ const initializeLogger = (loggingLevel, papertrailHost, papertrailPort, papertra
     transports,
     level: loggingLevel
   })
-
-  if (CLS && nodeEnv === 'production') {
-    logger.rewriters.push((level, msg, meta) => {
-      const session = CLS.get('session')
-      if (session) { meta.sId = session }
-      return meta
-    })
-  }
 
   logger.cli()
   logInfo[0] = `Log level is ${logger.level}`
