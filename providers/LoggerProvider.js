615c50cc8634c3c0b1229c85ad4cb6ca8fb46c69
Artem Golovin <agv.c1d@gmail.com>
2018-12-04 15:57:40 +0100

    Update logger to 3.0 and remove papertrail

--- providers/LoggerProvider.js
+++ providers/LoggerProvider.js
@@ -11,12 +11,9 @@ class LoggerProvider extends ServiceProvider {
     this.app.singleton('GoAbout/providers/Logger', () => {
       const Env = use('Env')
       const loggingLevel = Env.get('LOGGING', 'error')
-      const papertrailHost = Env.get('PAPERTRAIL_HOST', null)
-      const papertrailPort = Env.get('PAPERTRAIL_PORT', null)
-      const papertrailHostname = Env.get('PAPERTRAIL_HOSTNAME', null)
       const nodeEnv = Env.get('NODE_ENV', 'production')
       const CLS = use('GoAbout/providers/ContinuationLocalStorage')
-      return Logger(loggingLevel, papertrailHost, papertrailPort, papertrailHostname, nodeEnv, CLS)
+      return Logger(loggingLevel, nodeEnv, CLS)
     })
   }
 
