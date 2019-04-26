1c2811b5db41bd19afe3e597b80f1039f429e6d5
Artem Golovin <agv.c1d@gmail.com>
2019-04-26 14:19:44 +0200

    Disable cube-probes from appearing in logs

--- utils/RequestLoggerMiddleware.js
+++ utils/RequestLoggerMiddleware.js
@@ -15,7 +15,7 @@ class RequestLogger {
     }
 
     const userAgent = request.headers()['user-agent'] || 'Not defined'
-    if (userAgent.match(/pingdom|curl|pingdisco|uptimerobot/ig)) return next()
+    if (userAgent.match(/pingdom|curl|pingdisco|uptimerobot|kube-probe/ig)) return next()
 
     this.$Log.debug(`<!> Request to ${request.url()} with params ${stringifiedParams}, made by ${userAgent}, lang is ${request.headers()['accept-language'] || 'Not defined'} <!>`)
     return next()
