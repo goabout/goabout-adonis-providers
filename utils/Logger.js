86702fb7ffce289923d0f9794090214b6a588b1a
Artem Golovin <agv.c1d@gmail.com>
2019-06-05 15:55:28 +0200

    Send prod logs in json

--- utils/Logger.js
+++ utils/Logger.js
@@ -1,35 +1,33 @@
 const winston = require('winston')
 
 const initializeLogger = (loggingLevel, nodeEnv, CLS) => {
-  const printFormat = winston.format.printf(info => {
-    const prependers = []
-
-
-    if (CLS && nodeEnv === 'production') {
-      const session = CLS.get('session')
-      const userUid = CLS.get('userUid')
-      const clsData = `${userUid ? userUid.slice(0, 10) : ''}${userUid && session ? '-' : ' '}${session || ''}`
-      if (clsData) prependers.push(`\x1b[35m${clsData}\x1b[0m`)
+  const jsonFormatter = winston.format((info, opts) => {
+    if (CLS) {
+      info.session_id = CLS.get('session')
+      info.user_id = CLS.get('userUid')
     }
 
-    return `${prependers.join(' ')} ${info.level} ${info.message}`
-  })
-
-  const jsonFormatter = winston.format((info, opts) => {
-    info.session_id = CLS.get('session')
-    info.user_uid = CLS.get('useUid')
     return info
   })
 
-  const consoleFormats = [
+  // Log in string format for local
+  const localOutput = [
     winston.format.colorize(),
     winston.format.splat(),
-    printFormat
+    winston.format.simple()
   ]
+
+  // Log in json format for prod
+  const prodOutput = [
+    jsonFormatter(),
+    winston.format.json()
+  ]
+
+  const pickedOutput = nodeEnv === 'production' ? prodOutput : localOutput
 
   const transports = [
     new winston.transports.Console({
-      // format: winston.format.combine(...consoleFormats)
+      format: winston.format.combine(...pickedOutput)
     })
   ]
 
@@ -37,10 +35,6 @@ const initializeLogger = (loggingLevel, nodeEnv, CLS) => {
   const logInfo = [null]
 
   const logger = winston.createLogger({
-    format: winston.format.combine(
-      jsonFormatter(),
-      winston.format.json()
-    ),
     transports,
     level: loggingLevel
   })
